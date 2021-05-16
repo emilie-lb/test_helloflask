13ad5087d9b6c13308b07badd8e0c8269ac461a5
emilie-lb <emilielb75@gmail.com>
2021-05-16 19:06:14 +0200

    ajout app flask hello

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
   return "Hello"
 
 @app.route('/hello_world')
 def hello_world():
   return 'Hello World'
+
+
+if __name__ == '__main__':
+  app.run(debug = True, host = '0.0.0.0')
